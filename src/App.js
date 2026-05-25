2cdbfb96dba10f3a0c19851059e8f0f546a1b177
Alexmwale <alexmwalec03@gmail.com>
2026-05-25 19:17:12 +0200

    Fixing routing issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Router, Routes, } from 'react-router-dom';
+import { BrowserRouter, Router, Routes,Route} from 'react-router-dom';
 import Awards from './Pages/Awards/awards';
 import Publications from './Pages/Publications/Publications';
 import Education from './Pages/Education/Education';
@@ -9,17 +9,17 @@ import Projects from "./Pages/Projects/Projects";
 import Outreach from "./Pages/Outreach/Outreach";
 import Editorial from "./Pages/Editorial/Editorial";
 import Research  from "./Pages/Research/Research";
-import BoardMembership from "./Pages/BoardMembership/BoardMembership";
+import BoardMembership from "./Pages/BoardMembership/BoardMemberShip";
 import Teaching from "./Pages/Teaching/Teaching";
 import Role from  "./Pages/Role/Role";
-import Peer_Review from "./Pages/Peer_Review/Peer_Review";
+import Peer_Reviews from "./Pages/Peer_Reviews/Peer_Reviews";
 import Conference from "./Pages/Conference/Conference";
 import Blogs from "./Pages/Blogs/Blogs";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes > 
         <Route path="/" element={<Home/>} />
         <Route path="/awards" element={<Awards />} />
@@ -34,11 +34,11 @@ function App() {
         <Route path="/board-membership" element={<BoardMembership />} />
         <Route path="/teaching" element={<Teaching />} />
         <Route path="/role" element={<Role />} />
-        <Route path="/peer-review" element={<Peer_Review />} />
+        <Route path='/peer-review' element={<Peer_Reviews />} />
         <Route path="/conference" element={<Conference />} />
         <Route path="/blogs" element={<Blogs />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
